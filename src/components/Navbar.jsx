b61b03b7749d07ddbfff154e98e2e72b52cccbcf
Your Name <leeryan307@gmail.com>
2024-07-23 22:32:11 +0800

    done store component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,42 @@ import { bagImg, milkteaLogo, searchImg } from '../utils';
 import { navLists } from '../data/index.js';
 
 const Navbar = () => {
+    
+  const handleSmoothScroll = (e, link) => {
+    e.preventDefault();
+    const targetElement = document.querySelector(link);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <header className='navbar-container w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-        <nav className='navbar flex w-full'>
-            <img src={milkteaLogo} alt='Apple' className='w-6 lg:w-14'/>
+      <nav className='navbar flex w-full'>
+        <img src={milkteaLogo} alt='Apple' className='w-6 lg:w-14' />
 
-            <div className='category flex flex-1 justify-center max-sm:hidden'>
-                {navLists.map((nav, i) => (
-                    <div key={i} className='nav-link px-5 text-sm lg:text-2xl cursor-pointer text-gray-300 hover:text-white transition-all'>
-                        <a href={nav.link}>{nav.name}</a>
-                    </div>
-                ))}
+        <div className='category flex flex-1 justify-center max-sm:hidden'>
+          {navLists.map((nav, i) => (
+            <div
+              key={i}
+              className='nav-link px-5 text-sm lg:text-2xl cursor-pointer text-gray-300 hover:text-white transition-all'
+              onClick={(event) => handleSmoothScroll(event, nav.link)} // Add onClick handler
+            >
+              {nav.name}
             </div>
+          ))}
+        </div>
 
-            <div className='right-content flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
-                <img src={searchImg} alt='search' className='w-5 lg:w-10'/>
-                <img src={bagImg} alt='cart' className='w-5 lg:w-10'/>
-            </div>
-        </nav>
+        <div className='right-content flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
+          <img src={searchImg} alt='search' className='w-5 lg:w-10' />
+          <img src={bagImg} alt='cart' className='w-5 lg:w-10' />
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
